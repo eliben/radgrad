1cbb102baf10dd07f4a58987736aa797933122db
Eli Bendersky <eliben@gmail.com>
2025-01-15 06:04:17 -0800

    Appease ruff lint

--- radgrad/__init__.py
+++ radgrad/__init__.py
@@ -1,4 +1,9 @@
 from .radgrad import grad
-from . import numpy_wrapper
 
 __all__ = ["grad"]
+
+
+# Import this module just for its side effects, in case the user doesn't
+# import it directly. The redundant "as numpy_wrapper" alias is to avoid
+# ruff linter complaints.
+from . import numpy_wrapper as numpy_wrapper
